8e192c5191f9b54b0a61427fb9bd561f9e81d2e1
Jose Claudio da Silva Junior <jcsj.ti@gmail.com>
2023-10-14 09:56:18 +0000

    Update either.ts
    Improving either checks

--- src/shared/util/either.ts
+++ src/shared/util/either.ts
@@ -10,14 +10,12 @@ interface Right<B> {
 
 export type Either<A, B> = Left<A> | Right<B>;
 
-export function isLeft<A>(val: any): val is Left<A> {
-  if ((val as Left<A>).tag === "left") return true;
-  return false;
+export function isLeft<A, B>(val: Either<A, B>): val is Left<A> {
+  return val.tag === "left";
 }
 
-export function isRight<B>(val: any): val is Right<B> {
-  if ((val as Right<B>).tag === "right") return true;
-  return false;
+export function isRight<A, B>(val: Either<A, B>): val is Right<B> {
+  return val.tag === "right"
 }
 
 export function Left<A>(val: A): Left<A> {
